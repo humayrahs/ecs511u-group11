01ae20d91c04710d4dc3aa451b3aa5fd591618a7
humayrahs <humayrahsiddique@gmail.com>
2022-12-01 09:14:02 +0000

    added method

---
+++ sketch_dec1a/sketch_dec1a.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void switchMode(){
+  //switch anger/neut/friend mode
+}
+
+void ledEyes(){
+  //init led in setup, blink colour/patterns here
+}
+
+
